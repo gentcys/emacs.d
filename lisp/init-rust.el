6ee8ecb7c04f51d3136c30cf3e5f3fc3efb3c79c
gentcys <iamchencong@gmail.com>
2020-09-15 16:18:05 +0800

    Switch from racer to eglot in rust-mode

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -4,14 +4,7 @@
 
 
 (when (maybe-require-package 'rust-mode)
-  (when (maybe-require-package 'racer)
-    (add-hook 'rust-mode-hook #'racer-mode))
-  (when (maybe-require-package 'company)
-    (add-hook 'racer-mode-hook #'company-mode)))
-
-(when (maybe-require-package 'flycheck-rust)
-  (with-eval-after-load 'rust-mode
-    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
+  (add-hook 'rust-mode-hook 'eglot-ensure))
 
 (provide 'init-rust)
 ;;; init-rust.el ends here
